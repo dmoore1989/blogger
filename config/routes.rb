0ca1f79dd3d35c58de44442fef23cd4b617d5fb5
dmoore1989 <douglas.moore89@gmail.com>
2015-11-07 13:59:59 -0500

    Add header/Style

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+  root to: 'static_pages#home'
   resources :articles do
     resources :comments
   end
   resources :tags
   resources :authors
   resources :author_sessions, only: [ :new, :create, :destroy ]
-  
+
   get 'login'   =>  'author_sessions#new'
   get 'logout'  =>  'author_sessions#destroy'
-  
+  get 'about'   =>  'static_pages#about'
+  get 'games'   =>  'static_pages#games'
+
+
 end
